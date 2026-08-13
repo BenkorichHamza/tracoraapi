e8108779594c16329f753c5803964ab291d7e7c4
Hamza <hamzabenkorichetu@gmail.com>
2026-08-13 20:14:05 +0100

    fix(migration): update model_has_permissions table to use unsignedBigInteger for pivotPermission

--- database/migrations/2026_08_13_184059_create_permission_tables.php
+++ database/migrations/2026_08_13_184059_create_permission_tables.php
@@ -52,8 +52,9 @@ return new class extends Migration
         });
 
         Schema::create($tableNames['model_has_permissions'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission, $teams) {
-            $table->uuid($columnNames['model_morph_key']);
+            $table->unsignedBigInteger($pivotPermission);
             $table->string('model_type');
+            $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
             $table->foreign($pivotPermission)
