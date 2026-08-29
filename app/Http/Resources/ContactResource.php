92920e8a2bb87a2c552f16feaf178a51d1d3f6d7
Hamza <hamzabenkorichetu@gmail.com>
2026-08-29 10:00:23 +0100

    fix(api): update roles retrieval in ContactResource to use direct relationship

--- app/Http/Resources/ContactResource.php
+++ app/Http/Resources/ContactResource.php
@@ -119,7 +119,7 @@ class ContactResource extends JsonResource
 
 
             'user' => $this->whenLoaded('user'),
-            'roles' => $this->whenLoaded('user.roles'),
+            'roles' => $this->user->roles,
         ];
     }
 }
